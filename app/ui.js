0b19e5e49b3cf47a4413d225bc696307f13bfed5
aymammeri <ay.mammeri@gmail.com>
2022-01-15 17:34:03 -0500

    Add ui handlers for both upload and index
    Ui handler success and failure for onUpload and onGetIndex

--- app/ui.js
+++ app/ui.js
@@ -1,7 +1,7 @@
 const store = require('./store.js')
 
 const onSignUpSuccess = function () {
-  $('#signUp').trigger('reset')
+  $('#sign-up').trigger('reset')
   $('#message').text('User signed up successfully')
 }
 
@@ -9,20 +9,45 @@ const onSignUpFailure = function () {
   $('#message').text('Failed to sign up')
 }
 
-const onSignInSuccess = function (res) {
-  $('#signUp').trigger('reset')
+const onSignInSuccess = function (response) {
+  $('#sign-in').trigger('reset')
   $('#message').text('Signed-In successfully')
-  store.user = res.user
-  console.log(store.user)
+  store.user = response.user
+  console.log(store.user.token)
 }
 
 const onSignInFailure = function () {
   $('#message').text('Failed to Sign-In')
 }
 
+const onUploadSuccess = function (response) {
+  $('#upload')[0].reset()
+  $('#output').html(response)
+}
+
+const onUploadFailure = function (response) {
+  $('#output').html(response)
+}
+
+const onGetIndexSuccess = function (response) {
+  response.forEach(element => {
+    $('#files').append(
+      $('<li>', {}).text(element.name)
+    )
+  })
+}
+
+const onGetIndexFailure = function (response) {
+  $('#files').html('Error couldn\'t get files')
+}
+
 module.exports = {
   onSignUpSuccess,
   onSignUpFailure,
   onSignInSuccess,
-  onSignInFailure
+  onSignInFailure,
+  onUploadSuccess,
+  onUploadFailure,
+  onGetIndexSuccess,
+  onGetIndexFailure
 }
